63acdd6e85c6b0eb587723763d9dd0509ca48341
erik gomez <erik12fgtv@gmail.com>
2022-05-09 04:39:18 -0700

    spelling fix, add listener

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const h2 = document.createElement("h2");
 h2.textContent = "Feel free to take a look around.";
 document.querySelector("body").appendChild(h2);
+
+function changeColor() {
+    document.getElementById("list").style.backgroundColor = "purple"
+} 
+function setOldBackground() {
+    document.getElementById("list").style.backgroundColor = ""
+}
