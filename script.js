2ad1652f8a30419fee34c928c6b7459026daa137
erik gomez <erik12fgtv@gmail.com>
2022-05-09 02:14:48 -0700

    add more to style.css and index.html

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Feel free to take a look around.";
 document.querySelector("body").appendChild(h2);
